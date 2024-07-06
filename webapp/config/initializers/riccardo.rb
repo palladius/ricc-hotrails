a054be56a1e3c43b65b4c98acb14815d95d3839d
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-07-06 17:52:09 +0200

    Add Docker build steps and update file structure

---
+++ webapp/config/initializers/riccardo.rb
@@ -0,0 +1,5 @@
+
+
+APP_NAME = 'HotRails Riccardo Quotes'
+APP_DESCRIPTION = 'Taken from ... https://www.hotrails.dev/turbo-rails/turbo-frames-and-turbo-streams'
+APP_VERSION = File.read('VERSION').chomp
