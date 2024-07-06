c3dd5fccd19d038afe57f2d8669cd5be653ef343
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-07-06 18:28:59 +0200

    0.4a fixing Dockerfile and removing .yarn stuff

--- webapp/app/javascript/application.js
+++ webapp/app/javascript/application.js
@@ -1,6 +1,8 @@
 // Entry point for the build script in your package.json
 
-//import "@hotwired/turbo-rails"
+import "@hotwired/turbo-rails"
 //import "./controllers"
 //import { Turbo } from "@hotwired/turbo-rails"
 //Turbo.session.drive = false
+
+//import * as Turbo from "@hotwired/turbo"
