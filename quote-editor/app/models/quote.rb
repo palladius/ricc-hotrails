3122eb2f53a899e8bdec79b21977bd5542551e9f
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-07-07 09:59:58 +0200

    1.0final ch6 finished correctly

--- quote-editor/app/models/quote.rb
+++ quote-editor/app/models/quote.rb
@@ -8,12 +8,13 @@ class Quote < ApplicationRecord
 #  after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
 #  after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self } }
   # Syntactic cugar
-  after_create_commit -> { broadcast_prepend_to "quotes" }
-  after_update_commit -> { broadcast_replace_to "quotes" }
-  after_destroy_commit -> { broadcast_remove_to "quotes" }
+  # after_create_commit -> { broadcast_prepend_to "quotes" }
+  # after_update_commit -> { broadcast_replace_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
 
   # Ancora meglio:
-  # broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+#  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+  broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
 
 
 end
