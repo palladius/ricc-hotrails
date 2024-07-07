d4a4cac61fca5b0c9397d10e46f0e7e7f6136023
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-07-07 16:26:44 +0200

    1.3.7b I dont know why 1.3.6p8080 works

--- quote-editor/config/initializers/riccardo.rb
+++ quote-editor/config/initializers/riccardo.rb
@@ -6,6 +6,11 @@ APP_VERSION = File.read('VERSION').chomp
 MESSAGGIO_OCCASIONALE = ENV.fetch 'MESSAGGIO_OCCASIONALE', '🤷🏼‍♀️ Messaggio Non datur'
 
 Rails.application.config do |config|
+  # https://ricc-hotrails-v2-dev-x42ijqglgq-ew.a.run.app/
+  # https://ricc-hotrails-v2-prod-x42ijqglgq-ew.a.run.app/
+  config.hosts << "ricc-hotrails-v2-dev-x42ijqglgq-ew.a.run.app"
+  config.hosts << "ricc-hotrails-v2-prod-x42ijqglgq-ew.a.run.app"
+
   config.hosts << "ricc-hotrails-manhouse-dev-x42ijqglgq-ew.a.run.app"
   config.hosts << "ricc-hotrails-manhouse-prod-x42ijqglgq-ew.a.run.app"
   config.hosts << /ricc-hotrails-.*-x42ijqglgq-ew.a.run.app/
